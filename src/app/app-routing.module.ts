74b7f4cc306c9f8e1cfb3c47c54836c748e7357c
unknown <kristi.ingco@gmail.com>
2020-02-22 20:18:00 +0800

    Add routing to artisan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { ArtisanComponent } from './artisan/artisan.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: MainMenuComponent },
+  { path: 'artisan/:id', component: ArtisanComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
