50af8256f0013798adaf5ffc07a89a89d405e83e
unknown <kristi.ingco@gmail.com>
2020-02-22 21:43:32 +0800

    Add route to product page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { ArtisanComponent } from './artisan/artisan.component';
+import { ProductPageComponent } from './product-page/product-page.component';
 
 
 const routes: Routes = [
   { path: '', component: MainMenuComponent },
   { path: 'artisan/:id', component: ArtisanComponent },
+  { path: 'product/:id', component: ProductPageComponent },
 ];
 
 @NgModule({
