8b9e0628a5e417439b85fdcd06dc1b1415c08ca0
Alyssa Cuan <alyssa.cuan@obf.ateneo.edu>
2020-02-22 17:16:25 +0800

    Added product and artisan api endpoints

--- backend/server.js
+++ backend/server.js
@@ -34,9 +34,92 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://bluehacks2020:
   console.log("Database connection ready");
 
   // Initialize the app.
-  var server = app.listen(process.env.PORT || 4200, function () {
+  var server = app.listen(process.env.PORT || 3000, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
   });
 });
 
+// API ROUTES BELOW
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+  }
+
+  /*  "/api/artisans"
+   *    GET: returns all artisans
+   *    Required Params: NONE
+   */
+  app.get("/api/artisans", function(req, res) {
+    db.collection(ARTISANS_COLLECTION).find().toArray(function(err, docs) {
+        if (err) {
+            handleError(res, err.message, "Failed to get artisans.");
+        } else {
+            res.status(200).json(docs);
+        }
+    });
+  });
+  
+  /*  "/api/artisanById"
+   *    GET: returns an artisan from its artisan_id
+   *    Required Params: 
+   *    artisan_id=int
+   */
+  app.get("/api/artisanById", function(req, res) {
+    var artisan_id = req.query.artisan_id;
+    db.collection(ARTISANS_COLLECTION).find({artisan_id: artisan_id}).toArray(function(err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to get artisan.");
+        } else {
+            res.status(200).json(doc[0]);
+        }
+    });
+  });
+
+  /*  "/api/products"
+   *    GET: returns all products
+   *    Required Params: NONE
+   */
+  app.get("/api/products", function(req, res) {
+    db.collection(PRODUCTS_COLLECTION).find().toArray(function(err, docs) {
+        if (err) {
+            handleError(res, err.message, "Failed to get products.");
+        } else {
+            res.status(200).json(docs);
+        }
+    });
+  });
+
+  /*  "/api/productById"
+   *    GET: returns a product from its product_id
+   *    Required Params: 
+   *    product_id=int
+   */
+  app.get("/api/productById", function(req, res) {
+    var product_id = req.query.product_id;
+    db.collection(PRODUCTS_COLLECTION).find({product_id: product_id}).toArray(function(err, doc) {
+        if (err) {
+            handleError(res, err.message, "Failed to get product.");
+        } else {
+            res.status(200).json(doc[0]);
+        }
+    });
+  });
+
+  /*  "/api/productByArtisan"
+   *    GET: returns artisans that created the product
+   *    Required Params: 
+   *    artisan_id=int
+   */
+  app.get("/api/productByArtisan", function(req, res) {
+    var artisan_id = req.query.artisan_id;
+    db.collection(ARTISAN_PRODUCT_COLLECTION).find({artisan_id: artisan_id}).toArray(function(err, docs) {
+        if (err) {
+            handleError(res, err.message, "Failed to get artisan product keys.");
+        } else {
+            res.status(200).json(docs);
+        }
+    });
+  });
